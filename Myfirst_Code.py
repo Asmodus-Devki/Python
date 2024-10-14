fc6401217a3cd6dc5d8c2c34f014a0c38e543e56
Asmodus-Devki <168951345+Asmodus-Devki@users.noreply.github.com>
2024-10-14 18:20:56 +0530

    My SHopping APP.py

--- Myfirst_Code.py
+++ Myfirst_Code.py
@@ -1,14 +1,3 @@
-import http.server
-import socketserver
-
-PORT = 8000
-
-Handler = http.server.SimpleHTTPRequestHandler
-
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("Serving at port", PORT)
-    httpd.serve_forever()
-
 # My Shopping App...
 
 print("Welcome to the Devkinandan's Marketplace!")
